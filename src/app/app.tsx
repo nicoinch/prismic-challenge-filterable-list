7b6638dfe88c5516f34844854d425d4617136e78
Nicolas Inchauspe <nicolas.inchauspe@gmail.com>
2023-09-05 17:46:54 +0200

    :tada: Initial commit
    Contains the init of the application with the needed technical stack.

---
+++ src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="prismic-challenge-filterable-list" />
+    </div>
+  );
+}
+
+export default App;
